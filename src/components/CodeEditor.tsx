44d765fcd0d061d3d59f89c950a016795fabaf9e
nab138 <NicholasDanielSharp@gmail.com>
2024-05-21 09:35:26 -0400

    Start working on saving files

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -6,12 +6,25 @@ import { fs } from "@tauri-apps/api";
 
 export interface CodeEditorProps {
   file: string;
+  setUnsaved: (unsaved: boolean) => void;
+  focused: boolean;
 }
-export default ({ file }: CodeEditorProps) => {
+export default ({ file, setUnsaved, focused }: CodeEditorProps) => {
   const [editor, setEditor] =
     useState<monaco.editor.IStandaloneCodeEditor | null>(null);
   const monacoEl = useRef(null);
   const { mode } = useColorScheme();
+  const [originalText, setOriginalText] = useState("");
+
+  const saveFile = () => {
+    if (editor) {
+      const currentText = editor.getValue();
+      fs.writeFile({ path: file, contents: currentText }).then(() => {
+        setOriginalText(currentText);
+        setUnsaved(false); // Assuming setUnsaved is a toggle function
+      });
+    }
+  };
 
   useEffect(() => {
     let colorScheme = mode;
@@ -52,9 +65,27 @@ export default ({ file }: CodeEditorProps) => {
     if (editor) {
       fs.readTextFile(file).then((text) => {
         editor.setValue(text);
+        setOriginalText(text);
       });
     }
   }, [file, editor]);
+
+  useEffect(() => {
+    if (editor && originalText) {
+      const model = editor.getModel();
+      if (model) {
+        const listener = model.onDidChangeContent(() => {
+          if (editor.getValue() !== originalText) {
+            setUnsaved(true);
+          }
+        });
+
+        return () => {
+          listener.dispose();
+        };
+      }
+    }
+  }, [editor, setUnsaved, originalText]);
 
   return <div className={"code-editor"} ref={monacoEl}></div>;
 };
--- src/components/Tiles/Editor.tsx
+++ src/components/Tiles/Editor.tsx
@@ -10,6 +10,12 @@ export interface EditorProps {
 
 export default ({ openFiles, focusedFile }: EditorProps) => {
   const [tabNames, setTabNames] = useState<string[]>([]);
+  const [unsavedFiles, setUnsavedFiles] = useState<string[]>([]);
+  const [focused, setFocused] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (focusedFile !== null) setFocused(focusedFile);
+  }, [focusedFile]);
 
   useEffect(() => {
     const fetchTabNames = async () => {
@@ -27,15 +33,33 @@ export default ({ openFiles, focusedFile }: EditorProps) => {
         sx={{ height: "100%", overflow: "hidden" }}
         className={"editor-tabs"}
         defaultValue={focusedFile !== null ? openFiles.indexOf(focusedFile) : 0}
+        onChange={(_, newValue) => {
+          if (newValue === null) return;
+          setFocused(openFiles[newValue as number]);
+        }}
       >
         <TabList>
           {tabNames.map((name, index) => (
-            <Tab key={openFiles[index]}>{name}</Tab>
+            <Tab key={openFiles[index]}>
+              {name}
+              {unsavedFiles.indexOf(openFiles[index]) != -1 ? " •" : ""}
+            </Tab>
           ))}
         </TabList>
         {openFiles.map((file, index) => (
           <TabPanel value={index} key={index} sx={{ padding: 0 }}>
-            <CodeEditor key={file} file={file} />
+            <CodeEditor
+              focused={focused === file}
+              key={file}
+              file={file}
+              setUnsaved={(unsaved: boolean) => {
+                if (unsaved) setUnsavedFiles((unsaved) => [...unsaved, file]);
+                else
+                  setUnsavedFiles((unsaved) =>
+                    unsaved.filter((unsavedFile) => unsavedFile !== file)
+                  );
+              }}
+            />
           </TabPanel>
         ))}
       </Tabs>
