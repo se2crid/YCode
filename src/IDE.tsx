2fec1981cbfa53fc463d8c7701b9e7826424e857
nab138 <NicholasDanielSharp@gmail.com>
2024-05-22 12:12:55 -0400

    Add menu bar

--- src/IDE.tsx
+++ src/IDE.tsx
@@ -1,9 +1,10 @@
 import Splitter, { SplitDirection } from "@devbookhq/splitter";
-import CodeEditor from "./components/CodeEditor";
 import Tile from "./components/Tiles/Tile";
 import FileExplorer from "./components/Tiles/FileExplorer";
 import { useState } from "react";
 import Editor from "./components/Tiles/Editor";
+import MenuBar from "./components/Menu/MenuBar";
+import "./IDE.css";
 
 export interface IDEProps {
   openFolder: string;
@@ -14,20 +15,23 @@ export default ({ openFolder }: IDEProps) => {
   const [openFiles, setOpenFiles] = useState<string[]>([]);
 
   return (
-    <Splitter direction={SplitDirection.Horizontal} initialSizes={[20, 80]}>
-      <Tile>
-        <FileExplorer
-          openFolder={openFolder}
-          setOpenFile={(file: string) => {
-            setOpenFile(file);
-            setOpenFiles((oF) => [file, ...oF]);
-          }}
-        />
-      </Tile>
-      <Splitter direction={SplitDirection.Vertical} initialSizes={[70, 30]}>
-        <Editor openFiles={openFiles} focusedFile={openFile} />
-        <Tile title="Terminal">Terminal</Tile>
+    <div className="ide-container">
+      <MenuBar />
+      <Splitter direction={SplitDirection.Horizontal} initialSizes={[20, 80]}>
+        <Tile>
+          <FileExplorer
+            openFolder={openFolder}
+            setOpenFile={(file: string) => {
+              setOpenFile(file);
+              setOpenFiles((oF) => [file, ...oF]);
+            }}
+          />
+        </Tile>
+        <Splitter direction={SplitDirection.Vertical} initialSizes={[70, 30]}>
+          <Editor openFiles={openFiles} focusedFile={openFile} />
+          <Tile title="Terminal">Terminal</Tile>
+        </Splitter>
       </Splitter>
-    </Splitter>
+    </div>
   );
 };
---
+++ src/components/Menu/MenuBarButton.tsx
@@ -0,0 +1,64 @@
+import {
+  Dropdown,
+  DropdownProps,
+  ListItemButton,
+  MenuButton,
+  Theme,
+  menuItemClasses,
+  typographyClasses,
+} from "@mui/joy";
+import { cloneElement, forwardRef } from "react";
+
+type MenuBarButtonProps = Pick<DropdownProps, "children" | "open"> & {
+  onOpen: DropdownProps["onOpenChange"];
+  onKeyDown: React.KeyboardEventHandler;
+  menu: JSX.Element;
+  onMouseEnter: React.MouseEventHandler;
+};
+
+export default forwardRef(
+  (
+    { children, menu, open, onOpen, onKeyDown, ...props }: MenuBarButtonProps,
+    ref: React.ForwardedRef<HTMLButtonElement>
+  ) => {
+    return (
+      <Dropdown open={open} onOpenChange={onOpen}>
+        <MenuButton
+          {...props}
+          slots={{ root: ListItemButton }}
+          ref={ref}
+          role="menuitem"
+          variant={open ? "soft" : "plain"}
+        >
+          {children}
+        </MenuButton>
+        {cloneElement(menu, {
+          slotProps: {
+            listbox: {
+              id: `toolbar-example-menu-${children}`,
+              "aria-label": children,
+            },
+          },
+          placement: "bottom-start",
+          disablePortal: false,
+          variant: "soft",
+          sx: (theme: Theme) => ({
+            width: 288,
+            boxShadow: "0 2px 8px 0px rgba(0 0 0 / 0.38)",
+            "--List-padding": "var(--ListDivider-gap)",
+            "--ListItem-minHeight": "32px",
+            [`&& .${menuItemClasses.root}`]: {
+              transition: "none",
+              "&:hover": {
+                ...theme.variants.solid.primary,
+                [`& .${typographyClasses.root}`]: {
+                  color: "inherit",
+                },
+              },
+            },
+          }),
+        })}
+      </Dropdown>
+    );
+  }
+);
