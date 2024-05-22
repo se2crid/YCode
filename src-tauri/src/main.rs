526cdd1498324b0f3aed52f71d40f842ad662412
nab138 <NicholasDanielSharp@gmail.com>
2024-05-22 11:06:15 -0400

    Cleanup rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
-
+use std::process::{Command, Stdio};
 
 #[tauri::command]
 fn is_windows() -> bool {
@@ -15,7 +14,7 @@ async fn has_wsl() -> bool {
     if !is_windows() {
         return false;
     }
-    // Check for wsl
+
     let output = Command::new("wsl")
         .arg("echo")
         .arg("1")
@@ -24,10 +23,7 @@ async fn has_wsl() -> bool {
         .expect("failed to execute process");
 
     let output = String::from_utf8_lossy(&output.stdout);
-    if output.trim() == "1" {
-        return true;
-    }
-    false
+    return output.trim() == "1";
 }
 #[tauri::command]
 async fn has_theos() -> bool {
@@ -35,24 +31,22 @@ async fn has_theos() -> bool {
         if !has_wsl().await {
             return false;
         }
+
+        // Checks that $THEOS is set in wsl and that the directory contains theos's files
+        // For some reason, without cmd /C the command doesn't work properly. I'm guessing its some sort of quoting issue but I couldn't figure it out.
         let output = Command::new("cmd")
-        .args(&["/C", r#"bash -ic 'test -d $THEOS/extras ; echo $?'"#])
+            .args(&["/C", r#"bash -ic 'test -d $THEOS/extras ; echo $?'"#])
             .stdout(Stdio::piped())
             .output()
             .expect("failed to execute process");
 
         let stdout = String::from_utf8_lossy(&output.stdout);
 
-        if stdout.trim() == "0" {
-            return true;
-        }
-        return false;
+        return stdout.trim() == "0";
     }
-    // If $THEOS is set and the directory exists, return true
+    // On linux, can just check if $THEOS is set and the directory exists
     if let Ok(theos) = std::env::var("THEOS") {
-        if std::path::Path::new(&theos).exists() {
-            return true;
-        }
+        return std::path::Path::new(&theos).exists();
     }
     false
 }
@@ -61,58 +55,15 @@ async fn has_theos() -> bool {
 async fn update_theos(window: tauri::Window) {
     let mut command = if is_windows() {
         let mut cmd = Command::new("wsl");
-        cmd.arg("bash")
-            .arg("-ic")
-            .arg("'$THEOS/bin/update-theos'");
+        cmd.arg("bash").arg("-ic").arg("'$THEOS/bin/update-theos'");
         cmd
     } else {
         let mut cmd = Command::new("sh");
-        cmd.arg("-c")
-            .arg("$THEOS/bin/update-theos");
+        cmd.arg("-c").arg("$THEOS/bin/update-theos");
         cmd
     };
-    command.stdout(Stdio::piped());
-    let mut command = match command.spawn() {
-        Ok(cmd) => cmd,
-        Err(_) => {
-            window.emit("update-theos-output", "command.done.999".to_string()).expect("failed to send output");
-            return;
-        }
-    };
 
-    let output = match command.stdout.take() {
-        Some(out) => out,
-        None => {
-            window.emit("update-theos-output", "command.done.999".to_string()).expect("failed to send output");
-            return;
-        }
-    };
-
-    let reader = BufReader::new(output);
-
-    for line in reader.lines() {
-        match line {
-            Ok(line) => {
-                window.emit("update-theos-output", line).expect("failed to send output");
-            },
-            Err(_) => {
-                window.emit("update-theos-output", "command.done.999".to_string()).expect("failed to send output");
-                return;
-            }
-        }
-    }
-
-    let exit_status = match command.wait() {
-        Ok(status) => status,
-        Err(_) => {
-            window.emit("update-theos-output", "command.done.999".to_string()).expect("failed to send output");
-            return;
-        }
-    };
-
-    let exit_code = exit_status.code().unwrap_or(1);
-
-    window.emit("update-theos-output", format!("command.done.{}", exit_code)).expect("failed to send output");
+    pipe_command(&mut command, window, "update-theos").await;
 }
 
 #[tauri::command]
@@ -120,65 +71,22 @@ async fn install_theos_windows(handle: tauri::AppHandle, window: tauri::Window, 
     let resource_path = match handle.path_resolver().resolve_resource("install_theos.sh") {
         Some(path) => path,
         None => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
+            window
+                .emit("install-theos-output", "command.done.999".to_string())
+                .expect("failed to send output");
             return;
         }
     };
 
-    let resource_path_str = resource_path.display().to_string();
-    let drive_letter = resource_path_str[4..].chars().next().unwrap().to_ascii_lowercase();
-    let rest_of_path = resource_path_str[6..].replace("\\", "/");
-    let wsl_path = format!("/mnt/{}/{}", drive_letter, rest_of_path);
+    let wsl_path = windows_to_wsl_path(&resource_path.to_string_lossy());
     let mut command = Command::new("wsl");
-    command.arg("sh")
-        .arg("-c")
-        .arg(format!("export SUDO_PASSWORD={} ; tr -d '\r' < {} | bash", password, wsl_path));
-        
+    // Windows line endings are \r\n, so we need to remove the \r for bash to work properly
+    command.arg("sh").arg("-c").arg(format!(
+        "export SUDO_PASSWORD={} ; tr -d '\r' < {} | bash",
+        password, wsl_path
+    ));
 
-    command.stdout(Stdio::piped());
-
-
-    let mut command = match command.spawn() {
-        Ok(cmd) => cmd,
-        Err(_) => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
-            return;
-        }
-    };
-
-    let output = match command.stdout.take() {
-        Some(out) => out,
-        None => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
-            return;
-        }
-    };
-
-    let reader = BufReader::new(output);
-
-    for line in reader.lines() {
-        match line {
-            Ok(line) => {
-                window.emit("install-theos-output", line).expect("failed to send output");
-            },
-            Err(_) => {
-                window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
-                return;
-            }
-        }
-    }
-
-    let exit_status = match command.wait() {
-        Ok(status) => status,
-        Err(_) => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
-            return;
-        }
-    };
-
-    let exit_code = exit_status.code().unwrap_or(1);
-
-    window.emit("install-theos-output", format!("command.done.{}", exit_code)).expect("failed to send output");
+    pipe_command(&mut command, window, "install-theos").await;
 }
 
 #[tauri::command]
@@ -186,21 +94,32 @@ async fn install_theos(handle: tauri::AppHandle, window: tauri::Window) {
     let resource_path = match handle.path_resolver().resolve_resource("install_theos.sh") {
         Some(path) => path,
         None => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
+            window
+                .emit("install-theos-output", "command.done.999".to_string())
+                .expect("failed to send output");
             return;
         }
     };
 
     let mut command = Command::new("sh");
-        command.arg("-c")
+    command
+        .arg("-c")
         .arg(format!("bash {}", resource_path.display()));
 
-    command.stdout(Stdio::piped());
+    pipe_command(&mut command, window, "install-theos").await;
+}
 
-    let mut command = match command.spawn() {
+// Handles taking a command and piping the stdout and exit code to the window
+async fn pipe_command(cmd: &mut Command, window: tauri::Window, cmd_name: &str) {
+    let name = &format!("{}-output", cmd_name);
+    cmd.stdout(Stdio::piped());
+
+    let mut command = match cmd.spawn() {
         Ok(cmd) => cmd,
         Err(_) => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
+            window
+                .emit(name, "command.done.999".to_string())
+                .expect("failed to send output");
             return;
         }
     };
@@ -208,7 +127,9 @@ async fn install_theos(handle: tauri::AppHandle, window: tauri::Window) {
     let output = match command.stdout.take() {
         Some(out) => out,
         None => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
+            window
+                .emit(name, "command.done.999".to_string())
+                .expect("failed to send output");
             return;
         }
     };
@@ -218,10 +139,12 @@ async fn install_theos(handle: tauri::AppHandle, window: tauri::Window) {
     for line in reader.lines() {
         match line {
             Ok(line) => {
-                window.emit("install-theos-output", line).expect("failed to send output");
-            },
+                window.emit(name, line).expect("failed to send output");
+            }
             Err(_) => {
-                window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
+                window
+                    .emit(name, "command.done.999".to_string())
+                    .expect("failed to send output");
                 return;
             }
         }
@@ -230,19 +153,36 @@ async fn install_theos(handle: tauri::AppHandle, window: tauri::Window) {
     let exit_status = match command.wait() {
         Ok(status) => status,
         Err(_) => {
-            window.emit("install-theos-output", "command.done.999".to_string()).expect("failed to send output");
+            window
+                .emit(name, "command.done.999".to_string())
+                .expect("failed to send output");
             return;
         }
     };
 
     let exit_code = exit_status.code().unwrap_or(1);
 
-    window.emit("install-theos-output", format!("command.done.{}", exit_code)).expect("failed to send output");
+    window
+        .emit(name, format!("command.done.{}", exit_code))
+        .expect("failed to send output");
+}
+
+fn windows_to_wsl_path(path: &str) -> String {
+    let drive_letter = path[4..].chars().next().unwrap().to_ascii_lowercase();
+    let rest_of_path = path[6..].replace("\\", "/");
+    format!("/mnt/{}/{}", drive_letter, rest_of_path)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![has_theos, update_theos, install_theos, install_theos_windows, is_windows, has_wsl])
+        .invoke_handler(tauri::generate_handler![
+            has_theos,
+            update_theos,
+            install_theos,
+            install_theos_windows,
+            is_windows,
+            has_wsl
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
